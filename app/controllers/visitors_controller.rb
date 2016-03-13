f62780b6e91f3304b71c5f259e1a70fc93d2c620
Willy Ko <willyko@y7mail.com>
2016-03-13 06:19:36 -0700

    need to fix ajax notice

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,2 +1,31 @@
 class VisitorsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+    @visitor = Visitor.new(visitor_params)
+    respond_to do |format|
+      if @visitor.save
+        format.html {
+         redirect_to #index,
+         flash[:notice] = "Thank you for sharing"
+        }
+      else
+        format.html {
+         redirect_to #index,
+         flash[:alert] = "error"
+        }
+      end
+    end
+  end
+
+
+  private
+  def visitor_params
+    params.require(:visitor).permit(:name,:event_last,:event_interested,:email,:school, :event_next, :event_favourite, :comments)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  post '/visitor' => 'visitors#create'
+  get '/visitor' => 'visitors#index'
   root to: 'visitors#index'
 end
